3bb85e70dcd639c02d6624f7af01ab40513f8820
rafael_colin <rafael_colin@estudante.sesisenai.org.br>
2026-04-29 11:24:31 -0300

    add - js

---
+++ script/javascript.js
@@ -0,0 +1,26 @@
+function login{
+
+    document.getElementById("formLogin").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+   
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+    
+
+    if (!email.includes("@")) return alert("Email Inválido");
+    if (senha.length < 4) return alert("Senha Inválida! A senha deve possuir no mínimo 4 caracteres.");
+
+  
+
+
+
+
+
+
+
+
+    document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " + nome;
+});
+
+}
